9b0cf9e18461c8af44033e41927c4a89b762a431
MohamedElkilany <mohamedsayedelkilany@gmail.com>
2021-06-05 00:29:23 +0200

    Adding buttons edit and delete in table view style

--- Smart Notepad/Scenes/NotesList/Controller/NotesListVC+Function.swift
+++ Smart Notepad/Scenes/NotesList/Controller/NotesListVC+Function.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 extension NotesListVC {
     
@@ -24,34 +25,55 @@ extension NotesListVC {
         mainView.myNotesTable.register(UINib(nibName: MyNotesCell.cellID, bundle: nil), forCellReuseIdentifier:  MyNotesCell.cellID)
         mainView.myNotesTable.delegate = self
         mainView.myNotesTable.dataSource = self
-//        mainView.myNotesTable.allowsMultipleSelection = true
         mainView.myNotesTable.showsVerticalScrollIndicator = false
         mainView.myNotesTable.showsHorizontalScrollIndicator = false
-//        mainView.myNotesTable.separatorStyle = .none
     }
-    
-    
+
     
 }
 
 extension NotesListVC: UITableViewDelegate , UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 10
     }
-
+    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: MyNotesCell.cellID, for: indexPath) as!  MyNotesCell
-        if indexPath.row == 3 {
-            cell.noteBodyLab.text = "skdkvmkmvlmmlmalmkdmvkdasmvkvdsamkdskdmvslmdlskmkmsdakmlmvsadkmvsakmdsvskdkvmkmvlmmlmalmkdmvkdasmvkvdsamkdskdmvslmdlskmkmsdakmlmvsadkmvsakmdsv"
-        }
+        
         return cell
         
-}
+    }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return UITableView.automaticDimension
     }
+    
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteItem = UIContextualAction(style: .destructive, title: "Delete") {  (contextualAction, view, boolValue) in
+            print("delete")
+        }
+        
+        
+        let editItem = UIContextualAction(style: .normal, title: "Edit") {  (contextualAction, view, boolValue) in
+            view.backgroundColor = .blue
+            contextualAction.backgroundColor = .blue
+            
+            print("edit")
+        }
+        
+        
+        let swipeActions = UISwipeActionsConfiguration(actions: [deleteItem,editItem])
+        
+        return swipeActions
+    }
+    
     
     
     
