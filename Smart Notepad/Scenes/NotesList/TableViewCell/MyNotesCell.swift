40a480c491175c2e8a57b1528d4b1d7fc731b20b
MohamedElkilany <mohamedsayedelkilany@gmail.com>
2021-06-04 23:50:01 +0200

    Adding MyNotes Cell

---
+++ Smart Notepad/Scenes/NotesList/TableViewCell/MyNotesCell.swift
@@ -0,0 +1,25 @@
+//
+//  MyNotesCell.swift
+//  Smart Notepad
+//
+//  Created by Mohamed Elkilany on 04/06/2021.
+//
+
+import UIKit
+
+class MyNotesCell: UITableViewCell {
+
+    static let cellID = "MyNotesCell"
+    
+    @IBOutlet fileprivate weak var noteTitleLab: UILabel!
+    @IBOutlet fileprivate weak var noteBodyLab: UILabel!
+    @IBOutlet fileprivate weak var nearestLab: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        selectionStyle = .none
+    }
+    
+    
+    
+}
