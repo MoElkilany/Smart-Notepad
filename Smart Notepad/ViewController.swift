7ef6b072f451890cca0a262df54f92d5315df5f0
MohamedElkilany <mohamedsayedelkilany@gmail.com>
2021-06-04 21:52:30 +0200

    Initial Commit

---
+++ Smart Notepad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Smart Notepad
+//
+//  Created by Mohamed Elkilany on 04/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
